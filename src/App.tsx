7d57172f75eeb81b28fdd6f59c0c60a402a4803e
Igor Felipe <appsigor@gmail.com>
2022-11-13 03:20:47 -0300

    chore: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import { useState, useEffect } from "react";
+import { supabase } from "./config/supabaseClient";
+import Auth from "./pages/Auth";
+import Account from "./pages/Account";
+import { Session } from "@supabase/supabase-js";
 
-function App() {
+export default function App() {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container" style={{ padding: "50px 0 100px 0" }}>
+      {!session ? (
+        <Auth />
+      ) : (
+        <Account key={session.user.id} session={session} />
+      )}
     </div>
   );
 }
-
-export default App;
---
+++ src/pages/Account/index.tsx
@@ -0,0 +1,121 @@
+import { useState, useEffect } from "react";
+import Avatar from "../../components/Avatar";
+import { supabase } from "../../config/supabaseClient";
+
+const Account = ({ session }: { session: any }) => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [username, setUsername] = useState<string | null>(null);
+  const [website, setWebsite] = useState<string | null>(null);
+  const [avatar_url, setAvatarUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    getProfile();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [session]);
+
+  const getProfile = async () => {
+    try {
+      setLoading(true);
+      const { user } = session;
+
+      let { data, error, status } = await supabase
+        .from("profiles")
+        .select(`username, website, avatar_url`)
+        .eq("id", user.id)
+        .single();
+
+      if (error && status !== 406) {
+        throw error;
+      }
+
+      if (data) {
+        setUsername(data.username);
+        setWebsite(data.website);
+        setAvatarUrl(data.avatar_url);
+      }
+    } catch (error: any) {
+      alert(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const updateProfile = async (e: any) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+      const { user } = session;
+
+      const updates = {
+        id: user.id,
+        username,
+        website,
+        avatar_url,
+        updated_at: new Date(),
+      };
+
+      let { error } = await supabase.from("profiles").upsert(updates);
+
+      if (error) {
+        throw error;
+      }
+    } catch (error: any) {
+      alert(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div aria-live="polite">
+      {loading ? (
+        "Saving ..."
+      ) : (
+        <form onSubmit={updateProfile} className="form-widget">
+          <Avatar
+            url={avatar_url}
+            size={150}
+            onUpload={(url) => {
+              setAvatarUrl(url);
+              updateProfile({ username, website, avatar_url: url });
+            }}
+          />
+          <div>Email: {session.user.email}</div>
+          <div>
+            <label htmlFor="username">Name</label>
+            <input
+              id="username"
+              type="text"
+              value={username || ""}
+              onChange={(e: any) => setUsername(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="website">Website</label>
+            <input
+              id="website"
+              type="url"
+              value={website || ""}
+              onChange={(e: any) => setWebsite(e.target.value)}
+            />
+          </div>
+          <div>
+            <button className="button primary block" disabled={loading}>
+              Update profile
+            </button>
+          </div>
+        </form>
+      )}
+      <button
+        type="button"
+        className="button block"
+        onClick={() => supabase.auth.signOut()}
+      >
+        Sign Out
+      </button>
+    </div>
+  );
+};
+
+export default Account;
